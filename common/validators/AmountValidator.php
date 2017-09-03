b148e8fe93f8bb5733a5b9ee7d088254121cd33e
IlyaPlatov <platov.ilya@gmail.com>
2017-09-03 14:37:26 +0700

    Fix validator bug

--- common/validators/AmountValidator.php
+++ common/validators/AmountValidator.php
@@ -6,6 +6,6 @@ use yii\validators\RegularExpressionValidator;
 
 class AmountValidator extends RegularExpressionValidator
 {
-    public $pattern = '#\d+(\.\d{2})?#is';
+    public $pattern = '/^(?!0|\.00)[0-9]+(,\d{3})*(.[0-9]{0,2})$/';
     public $message = 'Allowed only 0000 or 0000.00 format.';
 }
